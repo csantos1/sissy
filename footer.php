690152d6bb340b5214d3b23481c1d847381be28f
Cecilia Santos <ceciliasantos@ceciliasantos.org>
2015-12-24 17:27:16 -0200

    adjusts layout

--- footer.php
+++ footer.php
@@ -14,11 +14,13 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+	  <div class="layout-container">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sissy' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'sissy' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'sissy' ), 'sissy', '<a href="http://ceciliasantos.com" rel="designer">Cecilia Santos</a>' ); ?>
 		</div><!-- .site-info -->
+	 </div><!-- .layout-container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- index.php
+++ index.php
@@ -14,6 +14,8 @@
 
 get_header(); ?>
 
+  <div class="layout-container">
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
